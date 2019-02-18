6531afa7451bd513b494fd5fe325ad3a66750100
Gilbert Torchon <gilbert@noukod.com>
2019-02-18 16:00:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 $(document).ready(function (){
 });
+
+function displayError(error){
+   $("#errors").html("error")
+}
+
+function searchRepositories(){
+  const searchTerms=$("#searchTerms").val();
+  if(searchTerms){
+    $.get(`https://api.github.com/search/repositories?q=${searchTerms}`,function(data){
+        console.log(JSON.stringify(data))
+        // let result=`<ul>${data.items.map((function(x)=>{
+        //   return `
+        //     <li>
+        //       <h2></h2>
+        //     </li>
+        //   `
+        // }))}
+        // </ul>`
+    })
+  }
+}
