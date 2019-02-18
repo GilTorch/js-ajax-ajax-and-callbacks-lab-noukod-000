a8d6b5ba59b69deabb0888d1f92b0e5c57e2c7f5
Gilbert Torchon <gilbert@noukod.com>
2019-02-18 16:12:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -38,7 +38,8 @@ function getCommits(element){
 
 function showCommits(data){
   //list the SHA, the author, the author's login, and the author's avatar as an image.
-
+  const newData=Array.prototype.slice.call(data);
+  console.log(newData)l
   let result=`<ul>${Array.prototype.slice.call(data).map((function(commit){
     return `
       <li>
