0fe0c26a340694d52022883dab22adbab21b96e4
Gilbert Torchon <gilbert@noukod.com>
2019-02-18 16:01:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,14 +10,21 @@ function searchRepositories(){
   if(searchTerms){
     $.get(`https://api.github.com/search/repositories?q=${searchTerms}`,function(data){
         console.log(JSON.stringify(data))
-        // let result=`<ul>${data.items.map((function(x)=>{
-        //   return `
-        //     <li>
-        //       <h2></h2>
-        //     </li>
-        //   `
-        // }))}
-        // </ul>`
+        let result=`<ul>${data.items.map((function(item){
+          return `
+            <li>
+              <div>
+                <img width="100" height="100" src="${item.owner.avatar_url}"/>
+              </div>
+              <h2><a href="${item.url}">${item.owner.login}</a>/${item.name}</h2>
+              <p>${item.description}</p>
+              <a href="#" data-username="${item.owner.login}" data-repository="${item.name}" onclick="getCommits(this)">Show Commits</a>
+            </li>
+          `
+        }))}.fail(displayError)
+        </ul>`;
+        $("#results").html(result);
     })
   }
 }
+
